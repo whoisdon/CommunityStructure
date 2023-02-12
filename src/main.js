9422e906f6dbb70ccdc426fa8d5f14b5db33cfe6
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-02-12 14:46:42 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,4 @@ const Client = require('./Handlers/client');
 const options = require('./Config/options');
 const client = new Client(options);
 
-require('./Utils/process')(client);
-
-module.exports = client;
-
 client.login(process.env.TOKEN);
