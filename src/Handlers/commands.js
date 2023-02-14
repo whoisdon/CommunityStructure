6107f3801bb17c3caee2f209aee713142e124723
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-02-14 18:15:35 -0300

    Update commands.js

--- src/Handlers/commands.js
+++ src/Handlers/commands.js
@@ -3,7 +3,7 @@ class Command {
     this.client = client;
     this.name = options.name || options.data.name;
     this.description = options.description || options.data.description;
-    this.options = options.options || options.data.options;
+    this.options = options.options || options.data?.options;
     this.permissions = options.permissions;
     this.onlyDevs = options.onlyDevs;
     this.defer = options.defer || true;
