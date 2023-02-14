dcf4d42da576121127a14958d59ac050b5938489
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-02-14 16:44:42 -0300

    Update commands.js

--- src/Handlers/commands.js
+++ src/Handlers/commands.js
@@ -1,9 +1,9 @@
 class Command {
   constructor(client, options) {
     this.client = client;
-    this.name = options.name;
-    this.description = options.description;
-    this.options = options.options;
+    this.name = options.name || options.data.name;
+    this.description = options.description || options.data.description;
+    this.options = options.options || options.data.options;
     this.permissions = options.permissions;
     this.onlyDevs = options.onlyDevs;
     this.defer = options.defer || true;
