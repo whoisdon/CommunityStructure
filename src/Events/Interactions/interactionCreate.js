ec192cb55bf03c57f26d539ce6e0cba1bc048248
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-02-17 12:02:18 -0300

    Update interactionCreate.js

--- src/Events/Interactions/interactionCreate.js
+++ src/Events/Interactions/interactionCreate.js
@@ -28,23 +28,30 @@ module.exports = class extends Events {
 
       if (!interaction.guild && command.permissions) throw new Error("[PERMISSIONS_COMMAND]: Cannot use 'permissions' in dm interactions")
       
-      if (!interaction.member.permissions.has(command.permissions)) {
-        return interaction.editReply({ content: 'Você não tem perm' });
-      } else if (!interaction.guild.members.me.permissions.has(command.permissions)) {
-        return interaction.editReply({ content: 'Eu não tenho perm bro' });
-      } else if (!this.client.cooldown.has(interaction.user.id)) {
+      if (command.permissions) {
+        if (!interaction.member.permissions.has(command.permissions)) {
+          return interaction.reply({ content: 'Você não tem perm' });
+        }
+        else if (!interaction.guild.members.me.permissions.has(command.permissions)) {
+          return interaction.reply({ content: 'Eu não tenho perm bro' });
+        }
+      }
+  
+      if (!this.client.cooldown.has(interaction.user.id)) {
         if (!command) {
-          return interaction.editReply({
+          return interaction.reply({
             content: 'Ocorreu um erro ao executar este comando...',
             ephemeral: true,
           });
-        } else command.run(interaction);
+        }
+        else command.run(interaction);
       } else {
-        return interaction.editReply({
+        return interaction.reply({
           content: 'Você está em cooldown, aguarde 5 segundos para usar os comandos novamente.',
           ephemeral: true,
         });
       }
+  
       await this.client.cooldown.add(interaction.user.id);
       setTimeout(async () => {
         await this.client.cooldown.delete(interaction.user.id);
