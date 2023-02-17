ea2ce39808428c25bc33641000ad8fe0235030ea
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-02-17 16:51:48 -0300

    Update commands.js

--- src/Handlers/commands.js
+++ src/Handlers/commands.js
@@ -1,13 +1,20 @@
 class Command {
   constructor(client, options) {
     this.client = client;
-    this.name = options.name || options.data?.name;
-    this.description = options.description || options.data?.description;
+    this.name = options.name || options.data.name;
+    this.description = options.description || options.data.description;
     this.options = options.options || options.data?.options;
     this.permissions = options.permissions;
     this.onlyDevs = options.onlyDevs;
     this.defer = options.defer || true;
+    this.dm_permission = false;
+    this.name_localizations = options.name_localizations;
+  }
+  
+  toJSON() {
+    const { client, ...data } = this;
+    return data;
   }
 }
 
-export default Command;
+module.exports = Command;
