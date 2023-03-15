058e01ca9f57bdff963e30531b29e459042f084d
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-03-15 16:06:09 -0300

    Update functions.js

---
+++ src/Utils/Functions.js
@@ -0,0 +1,12 @@
+const types = ['error', 'system', 'commands', 'firebase', 'mongoose', 'success', 'client'];
+
+export function logger(message, type) {
+    if (!type || types.indexOf(type) < 0)
+        type = types[0];
+    console.log(`(${types[types.indexOf(type)].charAt(0).toUpperCase() + types[types.indexOf(type)].slice(1)}) ${message}`);
+}
+export function msToDate(ms) {
+    ms = Math.round(ms / 1000);
+    const s = ms % 60, m = ~~((ms / 60) % 60), h = ~~((ms / 60 / 60) % 24), d = ~~(ms / 60 / 60 / 24);
+    return `${d}d:${h}h:${m}m:${s}s`;
+}
