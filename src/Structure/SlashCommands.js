474d7cb3a8739be78ce5191429341087a145c204
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-09-22 23:57:31 -0300

    Create SlashCommands.js

---
+++ src/Structure/SlashCommands.js
@@ -0,0 +1,26 @@
+import { PermissionFlagsBits } from 'discord.js';
+
+class SlashCommands {
+  constructor(client, options) {
+    this.client = client;
+    this.name = options.name || options.data.name;
+    this.description = options.description || options.data.description;
+    this.options = options.options || options.data?.options;
+    this.userPermissions = options.userPermissions;
+    this.botPermissions = options.botPermissions || [PermissionFlagsBits.SendMessages]; 
+    this.onlyDevs = options?.onlyDevs || false;
+    this.deferReply = options.deferReply || false;
+    this.guildCollection = options.guildCollection; 
+    this.emoji = this.client.emoji;
+    this.log = this.client.log;
+    this.quickdb = this.client.quickdb;
+    this.mysql = this.client.mysql;
+  }
+  
+  toJSON() {
+    const { client, botPermissions, quickdb, mysql, ...data } = this;
+    return data;
+  }
+}
+
+export default SlashCommands;
