8875fe6eac23e00912ae07aec538855c51119cb7
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-10-19 16:45:55 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
+import 'dotenv/config';
+
 import Client from './Handlers/client.js';
-
-import dotenv from 'dotenv'
-dotenv.config()
-
 import options from './Config/options.js';
+
 const client = new Client(options);
 
 client.login(process.env.TOKEN);
