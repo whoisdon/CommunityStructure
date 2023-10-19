32d763339a567dbd19de6b99ce5c299fd8cd5cb7
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-10-19 16:46:24 -0300

    Update AbyLaucher.js

--- src/AbyLaucher.js
+++ src/AbyLaucher.js
@@ -1,9 +1,8 @@
+import 'dotenv/config';
+
 import Client from './Structure/AbyClient.js';
-
-import dotenv from 'dotenv';
-dotenv.config()
-
 import options from './Config/Options.js';
+
 const client = new Client(options);
 
 client.login(process.env.TOKEN);
