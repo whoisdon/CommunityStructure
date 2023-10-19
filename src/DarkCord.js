fe6f5853e90832ce4f6255f92b578172ceef0e92
whoisdon <102386944+whoisdon@users.noreply.github.com>
2023-10-19 16:45:11 -0300

    Update DarkCord.js

--- src/DarkCord.js
+++ src/DarkCord.js
@@ -1,13 +1,11 @@
+import 'dotenv/config';
+
 import DarkCordClient from './Handlers/DarkCordClient.js';
-
-import dotenv from 'dotenv';
-dotenv.config();
-
 import ClientIntents from './Config/ClientIntents.js';
 
 const client = new DarkCordClient(process.env.TOKEN, {
   gateway: {
-    intents: ClientIntents,
+    intents: ClientIntents
   }
 });
 
